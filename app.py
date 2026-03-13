2b00dc39cbff826d37c6fd82de29355fcd2cbef6
IzabellaSilva <izabella.silva@cinbesa.com.br>
2026-03-13 14:13:05 -0300

    fix: update Docker image tag for ollama and adjust Kafka broker configuration in docker-compose

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def get_status(task_id):
     })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # host='0.0.0.0' é o que permite o acesso via Docker
+    app.run(host='0.0.0.0', port=5000,debug=True)
